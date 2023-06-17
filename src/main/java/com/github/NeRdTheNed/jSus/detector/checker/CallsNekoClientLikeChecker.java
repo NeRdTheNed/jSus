cf2f3c289450e6751e5cb1d0b052d4b57419ec2e
Ned Loynd <ned@loynd.com>
2023-06-18 00:01:26 +1000

    Bump sus level

--- src/main/java/com/github/NeRdTheNed/jSus/detector/checker/CallsNekoClientLikeChecker.java
+++ src/main/java/com/github/NeRdTheNed/jSus/detector/checker/CallsNekoClientLikeChecker.java
@@ -42,7 +42,7 @@ public class CallsNekoClientLikeChecker implements IChecker {
                                 final String UUIDLike = methodName.substring(1).replaceFirst("(\\p{XDigit}{8})(\\p{XDigit}{4})(\\p{XDigit}{4})(\\p{XDigit}{4})(\\p{XDigit}+)", "$1-$2-$3-$4-$5");
                                 UUID.fromString(UUIDLike);
                                 // Valid UUID: likely NekoClient
-                                res.add(new TestResult(TestResult.TestResultLevel.SUS, "Call to method " + methodOwner + "." + methodName + " found at class " + clazz.name));
+                                res.add(new TestResult(TestResult.TestResultLevel.STRONG_SUS, "Call to method " + methodOwner + "." + methodName + " found at class " + clazz.name));
                             } catch (final Exception e) {
                                 // Ignored
                             }
