4100bf6fd1548ff979fa57cdc3d8ff512252c199
Ned Loynd <ned@loynd.com>
2023-06-18 17:22:01 +1000

    Add another Base64 check

--- src/main/java/com/github/NeRdTheNed/jSus/detector/checker/Checkers.java
+++ src/main/java/com/github/NeRdTheNed/jSus/detector/checker/Checkers.java
@@ -360,6 +360,7 @@ public class Checkers {
         // TODO handle org.apache.commons.codec.binary.Base64
         // TODO handle org.apache.commons.codec.binary.Hex
         list.add(new CallsMethodChecker(Opcodes.INVOKEVIRTUAL, "java/util/Base64$Decoder", "decode", "(Ljava/lang/String;)[B", TestResult.TestResultLevel.BENIGN));
+        list.add(new CallsMethodChecker(Opcodes.INVOKESTATIC, "javax/xml/bind/DatatypeConverter", "parseBase64Binary", "(Ljava/lang/String;)[B", TestResult.TestResultLevel.BENIGN));
         //list.add(new CallsMethodChecker(Opcodes.INVOKEVIRTUAL, "java/lang/Process", "waitFor", "()I", TestResult.TestResultLevel.SUS));
         list.add(new CallsMethodChecker(Opcodes.INVOKESTATIC, "java/nio/file/Files", "setPosixFilePermissions", "(Ljava/nio/file/Path;Ljava/util/Set;)Ljava/nio/file/Path;", TestResult.TestResultLevel.BENIGN));
         list.add(new CallsMethodChecker(Opcodes.INVOKESTATIC, "java/nio/file/Files", "createSymbolicLink", "(Ljava/nio/file/Path;Ljava/nio/file/Path;[Ljava/nio/file/attribute/FileAttribute;)Ljava/nio/file/Path;", TestResult.TestResultLevel.BENIGN));
