b2a72b548d6290835ce70a125e7b1b327da4dfa4
Ned Loynd <ned@loynd.com>
2023-06-18 13:51:42 +1000

    Reduce log spam

--- src/main/java/com/github/NeRdTheNed/jSus/detector/checker/UncommonJVMInstructionChecker.java
+++ src/main/java/com/github/NeRdTheNed/jSus/detector/checker/UncommonJVMInstructionChecker.java
@@ -28,6 +28,7 @@ public class UncommonJVMInstructionChecker implements IChecker {
     @Override
     public List<TestResult> testClass(ClassNode clazz) {
         final List<TestResult> res = new ArrayList<>();
+        int foundNoOps = 0;
 
         for (final MethodNode methodNode : clazz.methods) {
             for (final AbstractInsnNode ins : methodNode.instructions) {
@@ -36,13 +37,17 @@ public class UncommonJVMInstructionChecker implements IChecker {
                 // TODO More functionality
                 switch (opcode) {
                 case Opcodes.NOP:
-                    res.add(new TestResult(TestResult.TestResultLevel.BENIGN, "Found uncommon JVM opcode " + opcodeName(opcode) + " at class " + clazz.name));
+                    foundNoOps++;
                     break;
 
                 default:
                     break;
                 }
             }
+        }
+
+        if (foundNoOps > 0) {
+            res.add(new TestResult(TestResult.TestResultLevel.BENIGN, "Found " + foundNoOps + " uncommon JVM opcode(s) " + opcodeName(Opcodes.NOP) + " at class " + clazz.name));
         }
 
         return res;
