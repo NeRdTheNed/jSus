d6939507691cf20ab840915ad0067b474c156e65
Ned Loynd <ned@loynd.com>
2023-06-22 15:12:19 +1000

    Add equality methods

--- src/main/java/com/github/NeRdTheNed/jSus/util/Pair.java
+++ src/main/java/com/github/NeRdTheNed/jSus/util/Pair.java
@@ -1,6 +1,27 @@
 package com.github.NeRdTheNed.jSus.util;
 
+import java.util.Objects;
+
 public class Pair<K, V> {
+    @Override
+    public int hashCode() {
+        return Objects.hash(k, v);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (!(obj instanceof Pair)) {
+            return false;
+        }
+
+        final Pair other = (Pair) obj;
+        return Objects.equals(k, other.k) && Objects.equals(v, other.v);
+    }
+
     public final K k;
     public final V v;
     public Pair(K k, V v) {
